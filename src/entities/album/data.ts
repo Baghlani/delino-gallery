7eaa84c308d76176aa7a3dbbbeba5878df0c3b5b
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-27 19:26:47 +0330

    add ISR revalidation fetchers | import types (look at #078da9a)

--- src/entities/album/data.ts
+++ src/entities/album/data.ts
@@ -1,6 +1,11 @@
+import { Album } from "./types";
+
 export const getAlbums = async (): Promise<Album[]> => {
   const albums = await fetch("https://jsonplaceholder.typicode.com/albums", {
     cache: "force-cache",
+    next: {
+      revalidate: 60 * 60,
+    },
   });
   return albums.json();
 };
--- src/entities/photo/data.ts
+++ src/entities/photo/data.ts
@@ -1,6 +1,11 @@
+import { Photo } from "./types";
+
 export const getPhotos = async (albumId: number): Promise<Photo[]> => {
   const photos = await fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`, {
     cache: "force-cache",
+    next: {
+      revalidate: 60 * 60,
+    },
   });
   return photos.json();
 };
@@ -8,6 +13,9 @@ export const getPhotos = async (albumId: number): Promise<Photo[]> => {
 export const getPhoto = async (id: number): Promise<Photo> => {
   const photo = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`, {
     cache: "force-cache",
+    next: {
+      revalidate: 60 * 60,
+    },
   });
   return photo.json();
 };
