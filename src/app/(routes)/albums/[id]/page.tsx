ac1d27cd8ec51d58dbe316413b9530b52d03997e
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-28 04:02:47 +0330

    add back button to the album page | refactor

--- src/app/(routes)/albums/[id]/page.tsx
+++ src/app/(routes)/albums/[id]/page.tsx
@@ -1,9 +1,11 @@
+import Button from "@/components/Button";
 import { getAlbum } from "@/entities/album/data";
 import { Album } from "@/entities/album/types";
 import { getPhotos } from "@/entities/photo/data";
 import { Photo } from "@/entities/photo/types";
 import Image from "next/image";
 import Link from "next/link";
+import { HiChevronLeft } from "react-icons/hi2";
 
 export const generateMetadata = async ({ params: { id } }: { params: { id: string } }) => {
   const album: Album = await getAlbum(+id);
@@ -19,10 +21,20 @@ export default async function AlbumPage({ params: { id } }: { params: { id: stri
 
   return (
     <div>
-      <h1 className="mb-4 text-xl md:text-2xl font-light text-white">Album #{id}: {album.title}</h1>
-      <div className="grid grid-cols-4 gap-4">
-        {photos.map(photo => (
-          <Link key={photo.id} href={`/photos/${photo.id}`} className="block relative aspect-square">
+      <Button type="outlined" href="/albums" className="mb-4 space-x-2 ps-2">
+        <HiChevronLeft className="size-4" />
+        <div>Back to albums</div>
+      </Button>
+      <h1 className="mb-4 font-light text-white sm:text-xl md:text-2xl">
+        Album #{id}: {album.title}
+      </h1>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
+        {photos.map((photo) => (
+          <Link
+            key={photo.id}
+            href={`/photos/${photo.id}`}
+            className="relative block aspect-square"
+          >
             <Image src={photo.url} alt={photo.title} fill className="object-cover" />
           </Link>
         ))}
