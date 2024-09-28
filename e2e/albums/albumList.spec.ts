7099164486dddf1a065674a90689173d36c202b7
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-28 17:07:25 +0330

    add two test for albums page

---
+++ e2e/albums/albumList.spec.ts
@@ -0,0 +1,20 @@
+import { expect, Page, test } from "@playwright/test";
+
+test.describe("Album List Page", () => {
+  test("should display a list of albums", async ({ page }: { page: Page }) => {
+    await page.goto("/albums");
+    const albumTitles = await page.locator("h2").allTextContents();
+    expect(albumTitles.length).toBeGreaterThan(0);
+  });
+
+  test("should navigate to album images page on album click", async ({ page }: { page: Page }) => {
+    await page.goto("/albums");
+    const firstAlbumLink = await page.locator("a", { hasText: "Album" }).first();
+    await firstAlbumLink.click();
+    const albumId = await firstAlbumLink
+      .getAttribute("href")
+      .then((href) => href?.split("/").pop());
+    await page.waitForURL("/albums/*");
+    expect(page.url()).toContain(`/albums/${albumId}`);
+  });
+});
--- src/app/(routes)/albums/page.tsx
+++ src/app/(routes)/albums/page.tsx
@@ -15,15 +15,15 @@ export default async function Albums() {
   return (
     <div className="pb-8">
       <div className="mb-4 space-y-2">
-        <h3 className="font-extraligh text-xl text-white md:text-2xl">
+        <h1 className="font-extraligh text-xl text-white md:text-2xl">
           Discover {albums.length} albums
-        </h3>
+        </h1>
       </div>
       <div className="mt-3 flex flex-col space-y-4 text-neutral-300 md:text-lg">
         {albums.map((album) => (
           <Link key={album.id} href={`/albums/${album.id}`}>
             <div className="font-mono text-[15px] text-neutral-100/50">Album #{album.id}</div>
-            <div>{album.title}</div>
+            <h2>{album.title}</h2>
           </Link>
         ))}
       </div>
