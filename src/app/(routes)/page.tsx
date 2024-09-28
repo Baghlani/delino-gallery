4d3f03c306f401134c9e509750013c06d5e419ad
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-28 04:03:29 +0330

    refactor first page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -3,15 +3,12 @@ import Button from "@/components/Button";
 export default async function Home() {
   return (
     <>
-      <div className="relative w-full h-full">
-        <div className="absolute w-full box-border space-y-5 top-1/2 -translate-y-1/2">
-          <h1
-            className="w-full text-center bg-gradient-to-tl from-blue-500 to-violet-600
-          text-4xl md:text-6xl lg:text-7xl font-bold bg-clip-text text-white/10"
-          >
+      <div className="relative h-full w-full text-center">
+        <div className="absolute top-1/2 box-border w-full -translate-y-1/2 space-y-5">
+          <h1 className="w-full bg-gradient-to-tl from-blue-500 to-violet-600 bg-clip-text text-center text-4xl font-bold text-white/10 md:text-6xl lg:text-7xl">
             Delino Gallery
           </h1>
-          <div className="text-center text-xl md:text-2xl lg:text-3xl text-neutral-400 font-light">
+          <div className="text-center text-xl font-light text-neutral-400 md:text-2xl lg:text-3xl">
             <span className="text-white">Discover and Enjoy</span> from 100 hand-picked albums
             <br className="hidden md:block" /> from the best Delino Artists.
           </div>
