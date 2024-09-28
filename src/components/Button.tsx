29dc2f5bcf2dd3a250f8032e155511ddb4865712
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-28 04:02:09 +0330

    add solid and outlined type to the Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,20 +3,31 @@ import Link from "next/link";
 
 type ButtonProps = {
   className?: string;
+  type?: "solid" | "outlined";
   children: React.ReactNode;
 } & ({ href: string; onClick?: never } | { onClick: () => void; href?: never });
 
 export default function Button({
   className,
+  type = "solid",
   href,
   onClick,
   children,
 }: ButtonProps) {
   const Component: React.ElementType = href ? Link : "button";
+  const baseClasses =
+    "inline-flex items-center justify-center rounded-md py-2 transition-all duration-300 space-x-2 hover:shadow-lg";
+  const solidClasses = "bg-white px-8 font-semibold text-black hover:shadow-white/50";
+  const outlinedClasses =
+    "self-start bg-transparent text-white ring-2 hover:shadow-[var(--tw-ring-color)] font-light px-4";
   return (
     <Component
       className={cn(
-        "mx-auto block w-fit rounded-md bg-white px-8 py-2 font-semibold text-black transition-all duration-300 hover:shadow-lg hover:shadow-white/50",
+        baseClasses,
+        {
+          [solidClasses]: type === "solid",
+          [outlinedClasses]: type === "outlined",
+        },
         className,
       )}
       {...(href ? { href } : { onClick })}
