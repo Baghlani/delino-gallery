76a616e922bb6e3109502a05bbedf201eff42793
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-09-30 22:43:59 +0330

    change text color

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
           <h1 className="w-full bg-gradient-to-tl from-blue-500 to-violet-600 bg-clip-text text-center text-4xl font-bold text-white/10 md:text-6xl lg:text-7xl">
             Delino Gallery
           </h1>
-          <div className="text-center text-xl font-light text-neutral-400 md:text-2xl lg:text-3xl">
+          <div className="text-center text-xl font-light text-white/60 md:text-2xl lg:text-3xl">
             <span className="text-white">Discover and Enjoy</span> from 100 hand-picked albums
             <br className="hidden md:block" /> from the best Delino Artists.
           </div>
