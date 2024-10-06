9976301763690737347e0d164f8fe5f4ccfc949b
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-10-06 15:13:32 +0330

    add some styles to selectBox

--- src/components/SelectBox.tsx
+++ src/components/SelectBox.tsx
@@ -15,7 +15,7 @@ export const SelectBox = ({
   return (
     <Select.Root defaultValue={selected} onValueChange={onChange}>
       <Select.Trigger
-        className="text-violet-200 data-[placeholder]:text-violet9 h-[35px] flex items-center justify-center gap-[5px] rounded bg-black px-[15px] text-[13px] leading-none shadow-[0_2px_10px] shadow-black/10 outline-none hover:bg-[#1b194d] focus:shadow-[0_0_0_2px] focus:shadow-black"
+        className="flex h-[35px] items-center justify-center gap-[5px] rounded bg-black px-[15px] text-[15px] leading-none text-violet-200 shadow-[0_2px_10px] shadow-black/10 outline-none transition-all duration-200 hover:bg-violet-800 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet-300"
         aria-label="Food"
       >
         <Select.Value placeholder={placeholder} />
@@ -25,14 +25,14 @@ export const SelectBox = ({
       </Select.Trigger>
       <Select.Portal>
         <Select.Content className="overflow-hidden rounded-md bg-black shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-          <Select.ScrollUpButton className="text-violet-200 flex h-[25px] cursor-default items-center justify-center bg-black">
+          <Select.ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center bg-black text-violet-200">
             <HiOutlineChevronUp />
           </Select.ScrollUpButton>
           <Select.Viewport className="p-[5px]">
             <Select.Item
               value="0"
               className={
-                "text-violet-200 data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1 relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none data-[disabled]:pointer-events-none data-[highlighted]:outline-none"
+                "relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[15px] leading-none text-violet-200 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-800 data-[disabled]:text-violet-900 data-[highlighted]:text-violet-100 data-[highlighted]:outline-none"
               }
             >
               <Select.ItemText>All users</Select.ItemText>
@@ -45,7 +45,7 @@ export const SelectBox = ({
                 key={item.value}
                 value={item.value}
                 className={
-                  "text-violet-200 data-[highlighted]:bg-violet9 data-[disabled]:text-mauve8 data-[highlighted]:text-violet1 relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[13px] leading-none data-[disabled]:pointer-events-none data-[highlighted]:outline-none"
+                  "relative flex h-[25px] select-none items-center rounded-[3px] pl-[25px] pr-[35px] text-[15px] leading-none text-violet-200 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet-800 data-[disabled]:text-violet-700 data-[highlighted]:text-violet-100 data-[highlighted]:outline-none"
                 }
               >
                 <Select.ItemText>{item.label}</Select.ItemText>
@@ -55,7 +55,7 @@ export const SelectBox = ({
               </Select.Item>
             ))}
           </Select.Viewport>
-          <Select.ScrollDownButton className="text-violet-200 flex h-[25px] cursor-default items-center justify-center bg-black">
+          <Select.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center bg-black text-violet-200">
             <HiOutlineChevronDown />
           </Select.ScrollDownButton>
         </Select.Content>
