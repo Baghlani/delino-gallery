0d08b235cd0fafba517743378918cf4d8c91b3f8
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-10-06 14:25:51 +0330

    show loading indicator while albums list is loading (after select a user)

--- src/app/(routes)/albums/components/UserSelect.tsx
+++ src/app/(routes)/albums/components/UserSelect.tsx
@@ -3,20 +3,28 @@
 import { SelectBox } from "@/components/SelectBox";
 import { User } from "@/entities/user/types";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useTransition } from "react";
+import { ImSpinner10 } from "react-icons/im";
 
-export function UserSelect({ users }: { users: User[] }) {
+export default function UserSelect({ users }: { users: User[] }) {
   const router = useRouter();
   const selectedUser = useSearchParams().get("userId");
+  const [isPending, startTransition] = useTransition();
 
   return (
-    <SelectBox
-      placeholder="Select a user"
-      items={users.map((user) => ({ value: user.id.toString(), label: user.name }))}
-      selected={selectedUser ?? "0"}
-      onChange={(value) => {
-        if (value !== "0") router.push(`/albums?userId=${value}`);
-        else router.push("/albums");
-      }}
-    />
+    <div className="flex items-center space-x-2">
+      <SelectBox
+        placeholder="Select a user"
+        items={users.map((user) => ({ value: user.id.toString(), label: user.name }))}
+        selected={selectedUser ?? "0"}
+        onChange={(value) => {
+          startTransition(() => {
+            if (value !== "0") router.push(`/albums?userId=${value}`);
+            else router.push("/albums");
+          });
+        }}
+      />
+      {isPending && <ImSpinner10 className="animate-spin text-xl text-white" />}
+    </div>
   );
 }
