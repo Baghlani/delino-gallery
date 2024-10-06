e2a34aba19f938a65590c460a18d066c95923852
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-10-06 15:13:08 +0330

    escape character

--- src/app/(routes)/albums/error.tsx
+++ src/app/(routes)/albums/error.tsx
@@ -6,7 +6,7 @@ export default function Error() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center space-y-5 text-white/90">
       <h2 className="text-4xl font-bold">Oops! there is an error</h2>
-      <p className="text-lg">we can't fetch the albums, pleasetry again later :/</p>
+      <p className="text-lg">we can&apos;t fetch the albums, pleasetry again later :/</p>
       <div className="flex space-x-2 max-[480px]:!mt-6 max-[480px]:flex-col max-[480px]:space-x-0 max-[480px]:space-y-2 max-[480px]:self-start">
         <Button onClick={() => (window.location.href = "/albums")}>View all Albums</Button>
         <Button onClick={() => window.location.reload()}>Refresh the page</Button>
