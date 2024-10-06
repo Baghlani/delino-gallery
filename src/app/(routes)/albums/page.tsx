84c5562b8a64cf95abb41d4e6a704d3f52db772f
Abolfazl Baghlani <afbaghlani@yahoo.com>
2024-10-06 14:24:56 +0330

    extract Albums list from the page and show not found if the userId is invalid

--- src/app/(routes)/albums/page.tsx
+++ src/app/(routes)/albums/page.tsx
@@ -1,24 +1,19 @@
 import { getUsers } from "@/entities/user/data";
-import Link from "next/link";
+import { notFound } from "next/navigation";
 import { getAlbums } from "../../../entities/album/data";
-import { UserSelect } from "./components/UserSelect";
-
-export const generateStaticParams = async () => {
-  const users = await getUsers();
-  return users.map((user) => ({ userId: user.id.toString() }));
-};
-
-export const dynamicParams = false;
+import AlbumsList from "./components/AlbumsList";
+import UserSelect from "./components/UserSelect";
 
 export const revalidate = 86400; //1 day in seconds
 
 export const generateMetadata = async ({
   searchParams: { userId },
 }: {
-  searchParams: { userId: string };
+  searchParams: Record<string, string | undefined>;
 }) => {
   const users = await getUsers();
   const userName = userId && users.find((user) => user.id === +userId)?.name;
+
   return {
     title: `${userName ? `${userName}'s ` : ""}Albums list | Delino Gallery`,
     description: `a list of Delino's albums ${userName && `from ${userName}`}`,
@@ -32,23 +27,19 @@ export default async function AlbumsListPage({
 }) {
   const albums = await getAlbums(+userId);
   const users = await getUsers();
+  const userName = userId && users.find((user) => user.id === +userId)?.name;
+
+  if (userId && !userName) return notFound();
+
   return (
     <div className="pb-8">
       <div className="mb-4 space-y-2">
         <h1 className="font-extraligh text-xl text-white md:text-2xl">
-          Discover {albums.length} albums{" "}
-          {userId && `from ${users.find((user) => user.id === +userId)?.name}`}
+          Discover {albums.length} albums {userId && `from ${userName}`}
         </h1>
       </div>
       <UserSelect users={users} />
-      <div className="mt-3 flex flex-col space-y-4 text-neutral-300 md:text-lg">
-        {albums.map((album) => (
-          <Link key={album.id} href={`/albums/${album.id}`} prefetch={true}>
-            <div className="font-mono text-[15px] text-neutral-100/50">Album #{album.id}</div>
-            <h2>{album.title}</h2>
-          </Link>
-        ))}
-      </div>
+      <AlbumsList albums={albums} />
     </div>
   );
 }
